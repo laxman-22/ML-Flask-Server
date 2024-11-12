4c7f65ef7800a74b5b33691106d201c866ce03d3
laxman-22 <69215570+laxman-22@users.noreply.github.com>
2024-11-12 00:26:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import joblib
 import pandas as pd
 from flask_cors import CORS
@@ -8,30 +8,33 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 model = joblib.load("titanic_survival_model.pkl")
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        try:
-            data = request.get_json()
-            pclass = data.get('pclass')
-            sex = data.get('sex')
-            age = data.get('age')
-        
-            sex = 0 if sex.lower() == 'male' else 1
+    try:
+        data = request.get_json()
+        pclass = data.get('pclass')
+        sex = data.get('sex')
+        age = data.get('age')
+    
+        sex = 0 if sex.lower() == 'male' else 1
 
-            input_data = pd.DataFrame([[pclass, sex, age]], columns=['pclass', 'sex', 'age'])
+        input_data = pd.DataFrame([[pclass, sex, age]], columns=['pclass', 'sex', 'age'])
 
-            survival_proba = model.predict_proba(input_data)[0][1]
-            survived = model.predict(input_data)[0]
-            return jsonify({
-                'survival_probability': survival_proba,
-                'prediction': 'Survived' if survived == 1 else 'Did not survive'
-            })
+        survival_proba = model.predict_proba(input_data)[0][1]
+        survived = model.predict(input_data)[0]
 
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-    else:
-        return jsonify({'message': 'This is a GET response'})
+        res = jsonify({
+            'survival_probability': survival_proba,
+            'prediction': 'Survived' if survived == 1 else 'Did not survive'
+        })
+        response = make_response(res)
+        response.set_cookie('your_cookie_name', 'cookie_value', samesite='None', secure=True)
+        response.headers.add("Access-Control-Allow-Origin", "https://your-next-app.vercel.app")
+ 
+        return response
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
